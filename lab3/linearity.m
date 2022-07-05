093b3cdf949a3140772fbb427f20973da72a9557
juju2181 <anishshilpakar8@gmail.com>
2022-07-05 21:24:53 +0545

    Updated linearity test

--- lab3/linearity.m
+++ lab3/linearity.m
@@ -12,17 +12,28 @@ a = fourierCoeff(x);
 % y[n]
 b = fourierCoeff(y);
 
+%Displaying the fourier coefficients for X and y 
+fprintf('Fourier Series of X gives a\na = ');
+disp(a);
+fprintf('Fourier Series of Y gives b\nb = ');
+disp(b);
 
+% Linearity test 
 rhs = A*a + B*b;
 lhs = A*x + B*y;
 
 lhs_coeff = fourierCoeff(lhs);
 
-% floor(lhs_coeff) == floor(rhs)
+fprintf('Linearity Test:\n');
+fprintf('LHS | Fourier Series of %dx + %dy: \n',A,B);
+disp(lhs_coeff);
+fprintf('RHS | %da + %db : \n', A, B);
+disp(rhs);
 
 if floor(abs(lhs_coeff - rhs)) == zeros(1,length(x));
-    "linear"
+    disp("Hence Fourier Series is linear");
 else 
-    "non-linear"
+    disp("Hence Fourier Series is non-linear");
 endif;
 
+disp("By Anish Shilpakar | KCE075BCT008");
